bd4cecf99b94f8139baa6fa0fe44d1cc95a058db
Victor Osés <victor.oses.rodrigues@hotmail.com>
2018-08-14 15:08:28 -0300

    Criação classe de usuarios e connectionString

---
+++ AutenticacaoUsuarios/Models/UsuariosContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutenticacaoUsuarios.Models
+{
+    public class UsuariosContext:DbContext
+    {
+        public UsuariosContext():base("Usuarios")
+        {
+
+        }
+
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
---
+++ AutenticacaoUsuarios/Startup1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(AutenticacaoUsuarios.Startup1))]
+
+namespace AutenticacaoUsuarios
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Authentication/Login")
+            });          
+        }
+    }
+}
