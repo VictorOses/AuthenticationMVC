b99637afec26fc84291a9d8833c2c494c5ec787f
Victor Osés <victor.oses.rodrigues@hotmail.com>
2018-08-15 11:19:12 -0300

    Criação de controllers Home e Autenticacao.

---
+++ AutenticacaoUsuarios/Controllers/AutenticacaoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutenticacaoUsuarios.Models;
+using AutenticacaoUsuarios.ViewModels;
+
+namespace AutenticacaoUsuarios.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        private UsuariosContext db = new UsuariosContext();
+
+        // GET: Autenticacao
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(CadastroUsuarioViewModel viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+
+            Usuario novoUsuario = new Usuario
+            {
+                Nome = viewmodel.Nome,
+                Login = viewmodel.Login,
+                Senha = viewmodel.Senha
+             };
+
+            db.usuarios.Add(novoUsuario);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ AutenticacaoUsuarios/ViewModels/CadastroUsuarioViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutenticacaoUsuarios.ViewModels
+{
+    public class CadastroUsuarioViewModel
+    {
+        [Required(ErrorMessage = "Informe seu nome")]
+        [MaxLength(100, ErrorMessage = "O nome deve ter até 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe seu login")]
+        [MaxLength(50, ErrorMessage = "Seu login deve ter até 50 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha")]
+        [DataType(DataType.Password)]
+        [MaxLength(6, ErrorMessage = "Sua senha deve ter no minímo 6 caracteres")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Confirme sua senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar senha")]
+        [MaxLength(6, ErrorMessage = "Sua senha deve ter no minímo 6 caracteres")]
+        [Compare(nameof(Senha), ErrorMessage = "A senha não corresponde ao que já foi informado")]
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
